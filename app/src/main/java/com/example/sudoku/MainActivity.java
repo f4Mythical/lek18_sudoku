9f62b2998912ccebffdfd6eb05a5fd16b46d5724
f4Mythical <grynka.adam@gmail.com>
2026-06-16 07:51:09 +0200

    add grid and test with create button

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sudoku;
 
 import android.os.Bundle;
+import android.sax.Element;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private GridLayout grid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        grid = findViewById(R.id.myGridLayout);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        createButton();
+
+    }
+    private void createButton(){
+        Button button = new Button(this);
+        button.setText("1");
+        grid.addView(button);
     }
 }
