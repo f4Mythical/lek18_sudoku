b8086918fe413ac102b67d805d3bddf9dd911498
f4Mythical <grynka.adam@gmail.com>
2026-06-16 08:11:08 +0200

    create gridlayout with edittext

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.sudoku;
 
 import android.os.Bundle;
-import android.sax.Element;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridLayout;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,24 +13,47 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private GridLayout grid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        grid = findViewById(R.id.myGridLayout);
-
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        grid = findViewById(R.id.myGridLayout);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        createButton();
 
+        createButton();
     }
-    private void createButton(){
-        Button button = new Button(this);
-        button.setText("1");
-        grid.addView(button);
+
+    private void createButton() {
+        for (int box = 0; box < 9; box++) {
+            GridLayout subGrid = new GridLayout(this);
+            subGrid.setColumnCount(3);
+            subGrid.setRowCount(3);
+
+            GridLayout.LayoutParams subGridParams = new GridLayout.LayoutParams();
+            subGridParams.setMargins(8, 8, 8, 8);
+            subGrid.setLayoutParams(subGridParams);
+
+            for (int i = 0; i < 9; i++) {
+                EditText editText = new EditText(this);
+                editText.setText(String.valueOf(i + 1));
+                editText.setId(View.generateViewId());
+                editText.setBackgroundResource(R.drawable.bg_btn_square);
+                GridLayout.LayoutParams btnParams = new GridLayout.LayoutParams();
+                btnParams.width = 110;
+                btnParams.height = 110;
+                editText.setLayoutParams(btnParams);
+
+                subGrid.addView(editText);
+            }
+            grid.addView(subGrid);
+        }
     }
 }
